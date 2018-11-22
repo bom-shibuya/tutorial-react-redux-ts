23c61edfa91436a031e52a06bdd0d5637c258e3b
bom-shibuya <19891636+bom-shibuya@users.noreply.github.com>
2018-11-22 20:14:08 +0900

    Add snap shot

--- src/stores/visibilityFilter/reducers.tsx
+++ src/stores/visibilityFilter/reducers.tsx
@@ -1,9 +1,8 @@
 import { Action, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
 
-export const visibilityFilter = (
-  state = VisibilityFilters.SHOW_ALL,
-  action: Action
-) => {
+export const initialState = VisibilityFilters.SHOW_ALL
+
+export const visibilityFilter = (state = initialState, action: Action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter
