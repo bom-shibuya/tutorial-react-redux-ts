033e7f3de2d81a267284fe1facbe0b298389bc2d
bom-shibuya <19891636+bom-shibuya@users.noreply.github.com>
2018-12-03 20:07:01 +0900

    Play styled-components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import './App.css'
 import Footer from './components/Footer'
 import AddTodo from './containers/AddTodo'
@@ -6,21 +7,28 @@ import Todos from './containers/Todos'
 
 import logo from './logo.svg'
 
+const AppWrapper = styled.div`
+  text-align: center;
+  background-color: red;
+`
+
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <AddTodo />
-        <Todos />
-        <Footer />
-      </div>
+      <AppWrapper>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <AddTodo />
+          <Todos />
+          <Footer />
+        </div>
+      </AppWrapper>
     )
   }
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import styled, { css } from 'styled-components'
 import { ITodo } from '../stores/todos'
 
 export interface IProps {
@@ -7,17 +8,59 @@ export interface IProps {
   i: number
 }
 
+const mq = {
+  base(range: string, args: TemplateStringsArray) {
+    return css`
+      @media ${range} {
+        ${css(args)};
+      }
+    `
+  },
+  min(size: string) {
+    return (args: TemplateStringsArray) =>
+      this.base(`(min-width: ${size})`, args)
+  },
+  max(size: string) {
+    return (args: TemplateStringsArray) =>
+      this.base(`(max-width: ${size})`, args)
+  },
+  between(minSize: string, maxSize: string) {
+    return (args: TemplateStringsArray) =>
+      this.base(`(min-width: ${minSize}) and (max-width: ${maxSize})`, args)
+  }
+}
+
+const StyledTodoItem = styled.li`
+  color: palevioletred;
+  font-size: 1.6rem;
+  line-height: 1.75;
+  background-color: ${(props: { i: number }) => props.i % 2 && 'blue'}
+  ${mq.max('767px')`
+    padding: 10px;
+  `}
+  ${mq.between('768px', '1024px')`
+    padding: 30px;
+  `}
+  ${mq.min('1025px')`
+    padding: 50px;
+  `}
+
+  &:hover {
+    font-size: 2rem;
+  }
+`
+
 const TodoItem = ({ todo, onClick, i }: IProps) => {
   const handleClick = () => onClick(i)
   return (
-    <li>
+    <StyledTodoItem i={i}>
       {todo.text}
       <input
         type="checkbox"
         defaultChecked={todo.complete}
         onClick={handleClick}
       />
-    </li>
+    </StyledTodoItem>
   )
 }
 
